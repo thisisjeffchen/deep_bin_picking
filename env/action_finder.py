da5d021381ef63a3471783a723edfb1cc7b6c468
Jeff Chen <jc1@stanford.edu>
2018-03-12 20:53:41 -0700

    add action finder, minor mods on single dqn

---
+++ env/action_finder.py
@@ -0,0 +1,24 @@
+import dexnet
+
+DEX_NET_PATH = '../dex-net/'
+DB_NAME = 'dexnet_2.hdf5'
+GRIPPER_NAME = 'yumi_metal_spline'
+GRIPPER_REL_PATH = 'data/grippers/'
+GRASP_METRIC = 'force_closure'
+DEFAULT_NUM_GRASPS_PER_ITEM = 3
+GRIPPER_Z_POS = 1
+FC_90_THRESHOLD = 0.010809481366594122
+
+class ActionFinder (object):
+	def __init__ (self):
+		pass
+
+	def find (state):
+		"""
+		Finds actions for the state
+		"""
+		pass
+
+
+if __name__ == "__main__":
+	#tests
